256959ea954c923a6b8958aa4e61fef27ab4d2c5
nsn <nexsoftnetwork@gmail.com>
2016-10-05 14:49:50 -0500

    Initial Commit

---
+++ FlickrClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrClient
+//
+//  Created by nsn on 10/5/16.
+//  Copyright © 2016 nex sn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
